bf61cba6c7533ff3e2ec148f9692b641c71989a1
MrAbdrakhman <99481973+MrAbdrakhman@users.noreply.github.com>
2022-04-29 15:12:22 +0600

    Update dreamteam2_4.py

--- dreamteam2_4.py
+++ dreamteam2_4.py
@@ -11,32 +11,37 @@
 # Если пользователь не угадал вы снова спросите у него число.
 # Если пользователь 3 раза подряд не угадал число, вы останавливаете цикл и говорите: "Вы проиграли..."
 #######################################################################
-# from random import random
-# my_number = round(random() * 100)
-# i = 1
-# print("Число сгенерировано. Попробуйте отгадать его.")
-# while i <= 3:
-#     u = int(input("Введите число: "))
-#     if u > my_number:
-#         print('Больше заданного')
-#     elif u < my_number:
-##         print('Меньше заданного')
-##    else: 
- #         print('Вы угадали!' % i)      
-##         break
-##    i += 1 
-## else:     
-##     print('Вы проиграли...Число -', my_number)     
+from random import random
+my_number = round(random() * 100)
+
+print("Число сгенерировано. Попробуйте отгадать его.")
+def gen(my_number):
+    i = 1
+    while i <= 3:
+        u = int(input("Введите число: "))
+        if u > my_number:
+            print('Больше заданного')
+        elif u < my_number:
+            print('Меньше заданного')
+        else:
+            print('Вы угадали!' % i)
+            break
+        i += 1
+    else:
+        print('Вы проиграли...Число -', my_number)
+gen(my_number)  
 # 
 # Задание 2:
 # Напишите программу которая циклично запрашивает 
 # у пользователя номера символов по таблице Unicode и выводит соответствующие им символы.
 # Завершает работу при вводе нуля.
-# while True:
-#     symbol = int(input("Enter the number of symbol: "))
-#     if symbol == 0:
-#        break
-#     print(chr(symbol))
+def nur():
+    while True:
+        symbol = int(input("Enter the number of symbol: "))
+        if symbol == 0:
+            break
+        print(chr(symbol))
+nur()
 #
 # Задание 3:
         # Напишите программу, которая измеряет длину введенной строки.
@@ -44,39 +49,41 @@
         # Если короче, то к строке добавляется столько символов *,
 # чтобы ее длина составляла десять символов, после чего новая строка должна выводиться на экран.
 ###################################################################
-# s = input("Введите: ")
-# if len(s) > 10:
-#     print("Длиннее десяти символов!")
-# else:
-#    for i in range(len(s), 10):
-#       s += "*"
-#    print(s)
+s = input("Введите: ")
+def len1(s):
+    if len(s) > 10:
+        print("Длиннее десяти символов!")
+    else:
+        for i in range(len(s), 10):
+            s += "*"
+        print(s)
+len1(s)
 
 # Задание 4:
         # Напишите программу, которая запрашивает у пользователя шесть вещественных чисел.
         # На экран выводит минимальное и максимальное из них, округленные до двух знаков после запятой.
         # Выполните задание без использования встроенных функций min() и max().
 ####################################################################
-# user = []
-# for i in range(6):
-#     user.append(float(input("Введите числа: ")))
-# maxx = user[0]
-# minn = user[0]
-# for i in user:
-#     if maxx > i:
-#         maxx = i
-#     else:
-#         minn = i
-# print(f"mах число: {round(maxx, 2)}, min число: {round(minn, 2)}")
+user = []
+for i in range(6):
+    user.append(float(input("Введите числа: ")))
+maxx = user[0]
+minn = user[0]
+for i in user:
+    if maxx > i:
+        maxx = i
+    else:
+        minn = i
+print(f"mах число: {round(minn, 2)}, min число: {round(maxx, 2)}")
 
 # Задание 5:
         # Напишите программу которая принимает
 # число любой длины и вытаскивает из него самое большое и самое маленькое число.
 ####################################################################
-# a = input('enter a number: ')
-# maximum = max(int(x) for x in str(a))
-# minimum = min(int(x) for x in str(a))
-# print('max number:', maximum, 'min number:', minimum)
+a = input('enter a number: ')
+maximum = max(int(x) for x in str(a))
+minimum = min(int(x) for x in str(a))
+print('max number:', maximum, 'min number:', minimum)
 
 
 
