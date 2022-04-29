460c3dd7aefddfc9f0e816be77de06511d3e00be
MrAbdrakhman <expert-sam@rambler.ru>
2022-04-29 13:13:35 +0600

    update

--- dreamteam2_4.py
+++ dreamteam2_4.py
@@ -81,7 +81,15 @@ Namespaces are one honking great idea --let's do more of those!
 #     Подсказка: используйте метод строчных значений, который проверяет, “заглавность” буквы.
 ##################################################################
 
-
+t2 =list(text.split())
+t3=[]
+print("Решение зад 6")
+def cap(a):
+    for i in a:
+        if i[0].istitle():
+            t3.append(i)
+    return t3
+print(cap(t2))
 
 
 
@@ -91,7 +99,22 @@ Namespaces are one honking great idea --let's do more of those!
 #     Напишите функцию, которая считайте его и возвратит список из слов, которые начинаются на букву “c” или “C”.
 #     Подсказка: используйте метод строчных значений, который проверяет, начинается ли слово на переданную букву.
 ####################################################################
-
+print('Решение зад 7')
+def creater_f(a):
+    with open('zen.txt', 'w+') as f:
+        f.write(a)
+        return f
+print(creater_f(text))
+list1=[]
+def check_c():
+    with open("zen.txt", 'r') as f:
+        txt = f.read()
+        txt2 = list(txt.split(' '))
+        for i in txt2:
+            if "c"==i[0] or 'C'==i[0]:
+                list1.append(i)
+        return  list1
+print(check_c())
 
 
 
@@ -100,8 +123,19 @@ Namespaces are one honking great idea --let's do more of those!
 #     Напишите код банкомата, который принимает цифру, выдает деньги с номиналом 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 3, 1.
 #     Подсказка: напишите функцию, используйте divmod()
 ##################################################################
-
-
+print('Решение зад 8')
+nominal=[5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 3, 1]
+money=int(input('введите сумму которую хотите получить с банкомата '))
+def bank(money, nominal):
+    s = 0
+    for i in nominal:
+        k=money//i
+        if k>0:
+            money=money-i*k
+            print('номинал: ',i,"количество купюр: ", k)
+            s=s+i*k
+    return s
+print("Итого вы получите: ",bank(money,nominal))
 
 
 
@@ -112,6 +146,16 @@ Namespaces are one honking great idea --let's do more of those!
 #                 аргументов внутренней функции.
 #     Подсказка: (outer_arg1 * outer_arg2) + (inner_arg1 / inner_arg2)
 ####################################################################
+print("Решение зад 9")
+a = int(input("введите первый аргумент "))
+b = int(input("введите второй аргумент "))
+
+def vnesh(a, b):
+    def vn(a, b):
+        print(a/b)
+        return a / b
+    print((a * b))
+    return vn(a, b) + (a * b)
 
 
 
@@ -125,21 +169,35 @@ Namespaces are one honking great idea --let's do more of those!
 #     То есть, это нужно проделать еще до фильтрации. 
 #     Функция, которая будет передана в filter() должна возвращать True, если в слове есть буква ‘p’.
 #####################################################################
-
-
+print('Решение зад 10')
+text2=list(text.split())
+def f1(word_p):
+    for i in range(len(word_p)):
+        if word_p[i]=='p':
+            return True
+    return False
+out_text=filter(f1,text2)
+print(list(out_text))
 
 
 # Задание 11:
 #     Дано
-    # dict_one = {'a':1, 'b':2, 'c':3}
-    # dict_two = {'d':4, 'e':5, 'f':6}
-    # dict_three = {'g':7, 'h':8, 'i':9}
-    # dict_four = {}
+dict_one = {'a':1, 'b':2, 'c':3}
+dict_two = {'d':4, 'e':5, 'f':6}
+dict_three = {'g':7, 'h':8, 'i':9}
+dict_four = {}
 #     С помощью цикла for необходимо собрать три первых словаря в словарь dict_four
 
 #     Подсказка: Для удобства итерации, первые три словаря можно записать в кортеж (dict_one, dict_two, dict_three
 #####################################################################
-
+print('Решение зад 11')
+a=tuple(dict_one.items())
+b=tuple(dict_two.items())
+c=tuple(dict_three.items())
+d=a+b+c
+for i in d:
+    dict_four.update({i[0]:i[1]})
+print(dict_four)
 
 
 
@@ -155,7 +213,19 @@ Namespaces are one honking great idea --let's do more of those!
 #     [1, 2, 3, 4, 5], сдвиг 2
 #     [3, 4, 5, 1, 2]
 #####################################################################
-
+print('Решение зад 12')
+a = list(map(int, input('Введите лементы списка через пробел ').split()))
+n= int(input('Введите значение для сдвига элементов '))
+print(a)
+def check_code(a,n):
+    if n>0:
+        for i in range(0,n):
+            a=a + [a.pop(0)]
+    elif n<0:
+        for i in range(0,n,-1):
+            a = [a.pop(-1)]+a
+    return a
+print(check_code(a,n))
 
 
 
@@ -164,16 +234,42 @@ Namespaces are one honking great idea --let's do more of those!
 #     Напишите программу, где исходный список содержит положительные и отрицательные числа. 
 #     Требуется положительные поместить в один список, а отрицательные - в другой.
 ###################################################################
-
-
+print('Решение зад 13')
+l1=[-5,2,6,8,-9]
+l2=[12,3,-4,6,-8,-5]
+lp=[]
+lo=[]
+def list_dist(a):
+    for i in a:
+        if i>0:
+            lp.append(i)
+        else:
+            lo.append(i)
+    return lp, lo
+list_dist(l1)
+list_dist(l2)
+print(lp,lo)
 
 
 # Задание 14:
 #     Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года, которому этот месяц принадлежит (зима, весна, лето или осень).
 #####################################################################
-
-
-
+print("Решение зад 14")
+n=int(input('Введите номер месяца '))
+vesna=[3,4,5]
+leto=[6,7,8]
+osen=[9,10,11]
+zima=[12,1,2]
+def sezon(a):
+    if a in vesna:
+        return 'весна'
+    if a in leto:
+        return 'лето'
+    if a in osen:
+        return 'осень'
+    if a in zima:
+        return 'зима'
+print(sezon(n))
 
 
 
